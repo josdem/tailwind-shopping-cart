20360d98b0b05f720c9d8de91542bd037fa39ffe
josdem <joseluis.delacruz@gmail.com>
2023-06-26 20:50:00 -0400

    Deleting semicolon

--- test/main.test.js
+++ test/main.test.js
@@ -1,4 +1,4 @@
-const main = require("../build/js/main.js");
+const main = require("../build/js/main.js")
 
 describe("validate item increment and decrement", () => {
   document.body.innerHTML = `
@@ -6,26 +6,26 @@ describe("validate item increment and decrement", () => {
         <input id="sku100" type="number" value="0"/>
         <button id="sku100p">+</button>
         <button id="sku100m">-</button>
-  `;
-  const input = document.querySelector("#sku100");
+  `
+  const input = document.querySelector("#sku100")
 
   test("should increment sku counter", () => {
-    const button = document.querySelector("#sku100p");
+    const button = document.querySelector("#sku100p")
     button.addEventListener("click", () => {
-      main.incrementValue("sku100p");
-    });
-    button.click();
+      main.incrementValue("sku100p")
+    })
+    button.click()
 
-    expect(input.value).toBe("1");
-  });
+    expect(input.value).toBe("1")
+  })
 
   test("should increment sku counter", () => {
     const button = document.querySelector("#sku100m");
     button.addEventListener("click", () => {
-      main.decrementValue("sku100m");
-    });
-    button.click();
+      main.decrementValue("sku100m")
+    })
+    button.click()
 
-    expect(input.value).toBe("0");
-  });
-});
+    expect(input.value).toBe("0")
+  })
+})
